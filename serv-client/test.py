2ad40eb51c3a7274b2a5783bbb25bd0595f95ef6
EnriqueGonzalezMartinez <70719724+EnriqueGonzalezMartinez@users.noreply.github.com>
2020-12-20 16:15:08 -0600

    Delete test.py

--- serv-client/test.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-#os.system('clear')
-#cmd = 'cls'
-#resultado = str(os.system('dir'))
-#res = subprocess.check_output('powershell ls', shell=True).decode()
-print(os.chdir('nada'))
