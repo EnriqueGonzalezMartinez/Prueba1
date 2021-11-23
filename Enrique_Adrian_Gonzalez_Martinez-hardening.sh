32853c10f06e238cca195eda343c11e23ad26f58
EnriqueGonzalezMartinez <70719724+EnriqueGonzalezMartinez@users.noreply.github.com>
2021-11-22 21:35:13 -0600

    examen

---
+++ Enrique_Adrian_Gonzalez_Martinez-hardening.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+var=$(cat /etc/*-release | grep ID=)
+# This if verifies that the OS is centos and the version is 7
+if [[ $var == *'ID="centos"'* && $var == *'VERSION_ID="7"'* ]]
+then
+	echo "One CentOS 7 was identified."
+	# This "if" checks if there is any ClamAV package installed to uninstall it.
+	if [[ -n $(yum list installed | grep clamav) ]]
+	then
+		sudo yum -y remove clamav*
+	fi
+	# Install the ClamAV and Epel packages
+	sudo yum install -y https://www.clamav.net/downloads/production/clamav-0.104.1.linux.x86_64.rpm
+	# Update all packages
+	sudo yum -y update
+# This if verifies that the OS is centos and the version is 8
+elif [[ $var == *'ID="centos"'* && $var == *'VERSION_ID="8"'* ]]
+then
+	echo "One CentOS 8 was identified."
+	# This "if" checks if there is any ClamAV package installed to uninstall it.
+	if [[ -n $(yum list installed | grep clamav) ]]
+	then
+		sudo yum -y remove clamav*
+	fi
+	# Install the ClamAV package
+	sudo yum install -y https://www.clamav.net/downloads/production/clamav-0.104.1.linux.x86_64.rpm
+	# Update all packages
+	sudo yum -y update
+
+else
+	echo "The operating system is not a Centos"
+fi
